74878b3a5cbc355b93c3d0382fecbd820d5eeb5c
Ivan C <cordovaivan8@gmail.com>
2022-05-15 21:47:38 -0700

    updated script.js

--- script.js
+++ script.js
@@ -10,23 +10,47 @@ function generatePassword(){
   });
   var numericCharacters = ["0","1","2","3","4","5","6","7","8","9"];
 
-  //prompt("Would you like to include special characters?", "yes");
-  //prompt("Would you like to include numeric characters?", "yes");
-  //prompt("Would you like to include lowercase characters?", "yes");
-  //prompt("Would you like to include uppercasse characters?", "yes");
-  //prompt("How many characters would you like in your password between 8 & 128?");
 
-  var includelowercasecharacter = prompt("Would you like to include lowercase characters?", "yes");
-  var includespecialcharacters = prompt("Would you like to include special characters?", "yes");
-  var includeuppercasecharacters = prompt("Would you like to include uppercasse characters?", "yes");
-  var includenumericcharacters = prompt("Would you like to include numeric characters?", "yes");
+  var includelowercasecharacter = prompt("Would you like to include lowercase characters?", "yes") == "yes";
+  console.log(includelowercasecharacter);
+  var includespecialcharacters = prompt("Would you like to include special characters?", "yes") == "yes";
+  var includeuppercasecharacters = prompt("Would you like to include uppercasse characters?", "yes") == "yes";
+  var includenumericcharacters = prompt("Would you like to include numeric characters?", "yes") == "yes";
   var passwordlength = prompt("How many characters would you like in your password between 8 & 128?");
+
+  let password = "";
+
+  while (password.length <= passwordlength) {
+    if (includelowercasecharacter) {
+        if (password.length == passwordlength) break;
+        password += lowerCasecharacters[Math.floor(Math.random() * lowerCasecharacters.length)];
+    }
+    if (includespecialcharacters) {
+        if (password.length == passwordlength) break;
+        password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+    }
+    if (includeuppercasecharacters) {
+        if (password.length == passwordlength) break;
+        password += upperCasecharacters[Math.floor(Math.random() * upperCasecharacters.length)];
+    }
+    if (includenumericcharacters) {
+        if (password.length == passwordlength) break;
+        password += numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
+    }
+  }
+
+  const shuffled = password.split('').sort(function(){return 0.5-Math.random()}).join('');
+
+  return shuffled;
+
+
 
   
 
 
   console.log(upperCasecharacters);
 }
+
 
 function writePassword() {
   var password = generatePassword();
